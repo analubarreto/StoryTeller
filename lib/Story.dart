d05f518f5999fbfde8afbe89946f3487cae7ab13
Ana Luiza <analuibm@gmail.com>
2020-10-09 22:20:00 -0700

    first commit

---
+++ lib/Story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storyTitle = '';
+  String choiceOne = '';
+  String choiceTwo = '';
+
+  Story(String title, String choiceOne, String choiceTwo) {
+    storyTitle = title;
+    choiceOne = choiceOne;
+    choiceTwo = choiceTwo;
+  }
+}
